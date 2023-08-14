06698b5a941d3df35e0c6b1a40a6fd6e9d3e4e03
six <github@sixis.ca>
2023-08-14 12:45:09 -0600

    refactor client to use Result

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,7 @@ use jsonrpsee::core::client::{Client, ClientT, SubscriptionClientT,
 use jsonrpsee::ws_client::WsClientBuilder;
 use jsonrpsee::rpc_params;
 use jsonrpsee::core::params::ObjectParams;
+use jsonrpsee::core::Error;
 
 use serde_json::{Map, Value};
 use serde::{Serialize, Deserialize};
@@ -87,13 +88,17 @@ pub fn connect() -> Subscription<Event> {
                                     let on_play: WsSubscription<Value>  = client
                                         .subscribe_to_method("Player.OnPlay")
                                         .await
-                                        .unwrap();
+                                        .expect(
+                                            "OnPlay Subscription should always work"
+                                        );
                                     notifications.insert("OnPlay", on_play);
 
                                     let on_stop: WsSubscription<Value>  = client
                                         .subscribe_to_method("Player.OnStop")
                                         .await
-                                        .unwrap();
+                                        .expect(
+                                            "OnStop Subscription should always work"
+                                        );
                                     notifications.insert("OnStop", on_stop);
 
                                     state = State::Connected(client, reciever);
@@ -112,31 +117,31 @@ pub fn connect() -> Subscription<Event> {
 
                         select! {
                             recieved = notifications.next() => {
-                                let (func, data) = recieved.unwrap();
+                                let (func, data) = recieved
+                                    .expect("select should always return data");
                                 // TODO: move this match to a different fn
                                 // TBH these notifications are kinda useless
                                 //     since I need to poll anyway
                                 match func {
                                     "OnPlay" => {
-                                        let info = data.unwrap();
-                                        dbg!(&info);
+                                        // note unwrap is 'safe' here due to select!
+                                        let info = data
+                                            .expect("select should always return data");
+                                        // dbg!(&info);
                                         let player = <ActivePlayer as Deserialize>::deserialize(
                                             &info["data"]["player"]
-                                        ).unwrap();
-
-                                        // possibly look at the item ID and get that if exists?
+                                        ).expect("OnPlay should contain a player item");
                                         
                                         let result = handle_kodi_command(
                                             KodiCommand::PlayerGetPlayingItem(player.playerid),
                                             client
                                         ).await;
-                                        let result = result.unwrap_or(Event::None);
-                                        if matches!(&result, &Event::Disconnected) {
+                                        if result.is_err() {
+                                            dbg!(result.err());
                                             state = State::Disconnected;
                                         } else {
-                                            let _ = output.send(result).await;
+                                            let _ = output.send(result.unwrap()).await;
                                         };
-
                                     }
                                     "OnStop" => {
                                         let nothing = PlayingItem::default();
@@ -152,33 +157,32 @@ pub fn connect() -> Subscription<Event> {
                             _ = poller.tick() => {
                                 // println!("Tick");
                                 let app_status = poll_kodi_application_status(client).await;
-                                let app_status = app_status.unwrap_or(Event::None);
-                                if matches!(&app_status, &Event::Disconnected) {
+                                if app_status.is_err() {
+                                    dbg!(app_status.err());
                                     state = State::Disconnected;
-                                    continue;
+                                    // if this fails there's no need to poll anything else
+                                    continue;   
                                 } else {
-                                    let _ = output.send(app_status).await;
+                                    let _ = output.send(app_status.unwrap()).await;
                                 }
                     
                                 let player_props = poll_kodi_player_status(client).await;
-                                let player_props = player_props.unwrap_or(
-                                    Event::UpdatePlayerProps(None)
-                                );
-                                if matches!(&player_props, &Event::Disconnected) {
+                                if player_props.is_err() {
+                                    dbg!(player_props.err());
                                     state = State::Disconnected;
                                 } else {
-                                    let _ = output.send(player_props).await;
+                                    let _ = output.send(player_props.unwrap()).await;
                                 }
                             }
                     
                             message = input.select_next_some() => {
                                 dbg!(&message);
                                 let result = handle_kodi_command(message, client).await;
-                                let result = result.unwrap_or(Event::None);
-                                if matches!(&result, &Event::Disconnected) {
+                                if result.is_err() {
+                                    dbg!(result.err());
                                     state = State::Disconnected;
                                 } else {
-                                    let _ = output.send(result).await;
+                                    let _ = output.send(result.unwrap()).await;
                                 }
                             }
                         }
@@ -192,92 +196,64 @@ pub fn connect() -> Subscription<Event> {
 
 }
 
-
+// Might change all these to return an Result instead and propagate the err
+// Result<Event, E>
 async fn poll_kodi_application_status (
     client: &mut Client,
-) -> Option<Event> {
-    let mut params = ObjectParams::new();
-    let _ = params.insert("properties", ["volume", "muted"]);
-
-    let response: Result<Value, _> = client.request(
+) -> Result<Event, Error> {
+    let response: Value = client.request(
         "Application.GetProperties",
-        params,
-    ).await;
-    if response.is_err() {
-        dbg!(response.err());
-        return Some(Event::Disconnected);
-    }
-    let res = response.unwrap();
-    let muted: bool = res["muted"].as_bool().unwrap();
-    let app_status = KodiAppStatus{muted: muted};
-    Some(Event::UpdateKodiAppStatus(app_status))
+        rpc_obj_params!("properties"=["volume", "muted"])
+    ).await?;
+    let muted: bool = response["muted"].as_bool().expect(
+        "`muted: bool` should exist in this response"
+    );
+    let app_status = KodiAppStatus{muted};
+    Ok(Event::UpdateKodiAppStatus(app_status))
 }
 
 
 async fn poll_kodi_player_status (
     client: &mut Client,
-) -> Option<Event> {
-    let response: Result<Value, _> = client.request(
+) -> Result<Event, Error> {
+    let response: Value = client.request(
         "Player.GetActivePlayers",
         rpc_params!(),
-    ).await;
-    if response.is_err() {
-        dbg!(response.err());
-        return Some(Event::Disconnected);
-    }
-    let res = response.unwrap();
-    let players = <Vec<ActivePlayer> as Deserialize>::deserialize(res).unwrap();
+    ).await?;
+   // let res = response.unwrap();
+    let players = 
+        <Vec<ActivePlayer> as Deserialize>::deserialize(response)
+        .expect("ActivePlayers should deserialize");
     
     if players.len() == 0 {
-        return None;
+        return Ok(Event::UpdatePlayerProps(None));
     }
     // For now only considering the first playerid it sees...
     let player_id = players[0].playerid;
 
-    let response: Result<Value, _> = client.request(
+    let response: Value = client.request(
         "Player.GetProperties",
         rpc_obj_params!{"playerid"=player_id, "properties"=PLAYER_PROPS},
-    ).await;
-    if response.is_err() {
-        dbg!(response.err());
-        return None;
-    } 
-    let res = response.unwrap();
+    ).await?;
     // dbg!(&res);
-    let mut playerprops = <PlayerProps as Deserialize>::deserialize(res).unwrap();
+    let mut playerprops = 
+        <PlayerProps as Deserialize>::deserialize(response)
+        .expect("GetProperties should deserialize");
     playerprops.player_id = Some(player_id);
-    Some(Event::UpdatePlayerProps(Some(playerprops)))
+
+    Ok(Event::UpdatePlayerProps(Some(playerprops)))
 }
 
 async fn handle_kodi_command(
     message: KodiCommand, 
     client: &mut Client
-) -> Option<Event> {
+) -> Result<Event, Error> {
 
     match message {
-        // TODO: likely make a generic "OKcommand" structure
-        //   that uses the match to determine what "RPC.method", [params]
-        //   then just use the same 'request' function and response 
-        //   for all of the buttons/etc that just return "OK"
-        //
-        // I could just fire and forget but I want to handle an error if any.
-        KodiCommand::Test => {
-            let response: Result<String, _> = client.request(
-                "GUI.ShowNotification", 
-                rpc_params!["test", "rust"],
-            ).await;
-
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            }
-            None
-            
-        }
         KodiCommand::GetDirectory{path, media_type: mediatype} => {
             // Episodes, Shows, Files, Movies.. 
             // probably going to call a separate thing to build the list here
-            let response: Result<Map<String, Value>, _> = client.request(
+            let response: Map<String, Value> = client.request(
                 "Files.GetDirectory",
                 rpc_params![
                     path, 
@@ -286,40 +262,27 @@ async fn handle_kodi_command(
                     DirSort{method:"date",order:"descending"} // TODO: SortType
                     ],
                 
-            ).await;
-
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            }
-
-            let res = response.unwrap();
+            ).await?;
             // dbg!(&res);
             let list = <Vec<DirList> as Deserialize>::deserialize(
-                    &res["files"]
-                ).unwrap();
+                    &response["files"]
+                ).expect("DirList should deserialize");
 
-            Some(Event::UpdateDirList(list))
+            Ok(Event::UpdateDirList(list))
         }
 
         KodiCommand::GetSources(mediatype) => {
-            let response: Result<Map<String, Value>, _> = client.request(
+            let response: Map<String, Value> = client.request(
                 "Files.GetSources",
                 rpc_params![mediatype.as_str()],
-            ).await;
+            ).await?;
 
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            } 
-
-            let res = response.unwrap();
             let sources: Vec<Sources> = 
                 <Vec<Sources> as Deserialize>::deserialize(
-                    &res["sources"]
-                ).unwrap();
+                    &response["sources"]
+                ).expect("Sources should deserialize");
             
-            Some(Event::UpdateSources(sources))
+            Ok(Event::UpdateSources(sources))
         }
 
         KodiCommand::PlayerOpen(file) => {
@@ -327,95 +290,85 @@ async fn handle_kodi_command(
             struct Item{ file : String }
             let objitem = Item{file};
 
-            let response: Result<String, _> = client.request(
+            let response: String = client.request(
                 "Player.Open",
                 rpc_obj_params!{"item"=objitem},
-            ).await;
+            ).await?;
+            if response != "OK" {dbg!(response);};
 
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            }
-            None
+            Ok(Event::None)
         }
 
         KodiCommand::InputButtonEvent{button, keymap} => {
-            let response: Result<String, _> = client.request(
+            let response: String = client.request(
                 "Input.ButtonEvent",
                 rpc_obj_params!{"button"=button, "keymap"=keymap},
-            ).await;
+            ).await?;
 
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            }
-            None
+            if response != "OK" {dbg!(response);};
+
+            Ok(Event::None)
         }
 
         KodiCommand::InputExecuteAction(action) => {
-            let response: Result<String, _> = client.request(
+            let response: String = client.request(
                 "Input.ExecuteAction",
                 rpc_params![action],
-            ).await;
+            ).await?;
 
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            }
-            None
+            if response != "OK" {dbg!(response);};
+
+            Ok(Event::None)
         }
 
         KodiCommand::PlayerGetPlayingItem(player_id) => {
-            let response: Result<Map<String, Value>, _> = client.request(
+            let response: Map<String, Value> = client.request(
                 "Player.GetItem",
                 rpc_obj_params!{
                     "playerid"=player_id, 
                     "properties"=PLAYING_ITEM_PROPS
                 },
-            ).await;
-            if response.is_err() {
-                dbg!(response.err());
-                return Some(Event::Disconnected);
-            }
-            let response = response.unwrap();
-            // dbg!(&response);
+            ).await?;
+
             let playing_item = <PlayingItem as Deserialize>::deserialize(
                 &response["item"]
-            ).unwrap();
+            ).expect("PlayingItem should deserialize");
 
-            Some(Event::UpdatePlayingItem(playing_item))
+            Ok(Event::UpdatePlayingItem(playing_item))
             
         }
         
         // Debug command
         KodiCommand::PlayerGetActivePlayers => {
-            let response: Result<Value, _> = client.request(
+            let response: Value = client.request(
                 "Player.GetActivePlayers",
                 rpc_params!(),
-            ).await;
-            if response.is_err() {
-                dbg!(response.err());
-            } else {
-                dbg!(response.unwrap());
-            }
-            None
+            ).await?;
+            dbg!(response);
+            Ok(Event::None)
         }
 
         // Debug command
         KodiCommand::PlayerGetProperties => {
-            let response: Result<Value, _> = client.request(
+            let response: Value = client.request(
                 "Player.GetProperties",
                 rpc_obj_params!{"playerid"=1, "properties"=PLAYER_PROPS},
-            ).await;
-            if response.is_err() {
-                dbg!(response.err());
-            } else {
-                let res = response.unwrap();
-                dbg!(&res);
-                let props = <PlayerProps as Deserialize>::deserialize(res).unwrap();
-                dbg!(props);
-            }
-            None
+            ).await?;
+            dbg!(&response);
+            let props = 
+                <PlayerProps as Deserialize>::deserialize(response)
+                .expect("PlayerProps should deserialize");
+            dbg!(props);
+            Ok(Event::None)
+        }
+        // Debug command
+        KodiCommand::Test => {
+            client.request(
+                "GUI.ShowNotification", 
+                rpc_params!["test", "rust"],
+            ).await?;
+            //dbg!(response);
+            Ok(Event::None)   
         }
     }
   //  None
@@ -440,5 +393,5 @@ pub enum Event {
     UpdateDirList(Vec<DirList>),
     UpdatePlayerProps(Option<PlayerProps>),
     UpdateKodiAppStatus(KodiAppStatus),
-    UpdatePlayingItem(PlayingItem)
+    UpdatePlayingItem(PlayingItem) // Might change to Option
 }
